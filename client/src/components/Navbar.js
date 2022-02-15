ea41a4ad9e55e6dbc08af9f21d4e10536c4c8331
aka773 <alkaloid77@naver.com>
2022-02-15 15:11:25 +0900

    searchbar changed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import Search from './Search';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
 	return (
 			<div className="navbar">
-					<input placeholder="Search" className="search_bar"></input>
-					<button className="search_button">🔍</button>
+					<Search />
 				<ul className="link">
 					<Link to="/explore" style={{ textDecoration: 'none', color: 'inherit' }}><li>Explore</li></Link>
 					<Link to="/create" style={{ textDecoration: 'none', color: 'inherit' }}><li>Create</li></Link>
