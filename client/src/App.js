d9a28a6d19e9c9673cdcb0ebd60ff5d90345b8c5
aka773 <alkaloid77@naver.com>
2022-02-18 12:11:47 +0900

    app.js changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Create from './pages/Create';
@@ -7,6 +9,21 @@ import { Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
+
+	const [web3, setWeb3] = useState();
+	
+    useEffect(() => {
+        if (typeof window.ethereum !== "undefined") { // window.ethereum이 있다면
+            try {
+                const web = new Web3(window.ethereum);  // 새로운 web3 객체를 만든다
+                setWeb3(web);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }, []);
+
+
 	return (
 		<BrowserRouter>
 			<div className="App">
